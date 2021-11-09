ea8daa558eb66690e5e1f737f7b84a0e92ec0dc8
ananya-01 <ananya200120@gmail.com>
2021-11-09 18:19:59 +0530

    Add Restaurant Routes

---
+++ server/API/Restaurant/index.js
@@ -0,0 +1,72 @@
+//libraries
+import express from "express";
+
+//database Model
+import {RestaurantModel} from "../../database/allModel";
+
+
+const Router = express.Router();
+
+/* 
+Route           /
+Desc            Get all restaurant based on city name
+Params          none
+Access          public
+Method          GET
+*/
+Router.get("/", async (req, res) => {
+    try{
+        const { city } = req.query;
+        const restaurants = await RestaurantModel.find({ city }); 
+        return res.json({restaurants});
+    }
+    catch(error){
+        return res.status(500).json({error: error.message});
+    }
+});
+
+/* 
+Route           /:_id
+Desc            Get individual restaurant details based on id
+Params          _id
+Access          public
+Method          GET
+*/
+Router.get("/:_id", async (req, res) => {
+    try{
+        const {_id} = req.params;
+        const restaurant = await RestaurantModel.findById(_id);
+        if(!restaurant){
+            return res.status(404).json({error: "Restaurant not found"});
+        }
+        return res.json({restaurant});
+    }
+    catch(error){
+        return res.status(500).json({error: error.message});
+    }
+});
+
+/* 
+Route           /search
+Desc            Get restaurant details based on searched string
+Params          none
+Access          public
+Method          GET
+*/
+Router.get("/search",async (req, res) => {
+    try{
+        const {searchString } = req.body;
+        const restaurants = await RestaurantModel.find({
+            name: { $regex: searchString, $options: "i" },
+        });
+        if(!restaurants){
+            return res.status(404).json({error: `No Restaurants matched with ${searchString}`})
+        }
+        return res.json({restaurants});
+    }
+    catch(error){
+        return res.status(500).json({error: error.message});
+    }
+});
+
+export default Router;
