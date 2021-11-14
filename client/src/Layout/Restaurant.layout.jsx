86a7cc95cadc6eef6a1a06c7bd39b4827e12cee5
ananya-01 <ananya200120@gmail.com>
2021-11-14 09:59:08 +0530

    Add restaurant info

--- client/src/Layout/Restaurant.layout.jsx
+++ client/src/Layout/Restaurant.layout.jsx
@@ -1,8 +1,13 @@
 import React from "react";
+import { TiStarOutline } from "react-icons/ti";
+import { RiDirectionLine, RiShareForwardLine } from "react-icons/ri";
+import { BiBookmarkPlus } from "react-icons/bi";
 
 //components
 import RestaurantNavbar from "../components/Navbar/restaurantNavbar";
 import ImageGrid from "../components/restaurant/ImageGrid";
+import InfoButtons from "../components/restaurant/InfoButtons";
+import RestaurantInfo from "../components/restaurant/RestaurantInfo";
 
 const RestaurantLayout = () => {
     return(
@@ -18,6 +23,26 @@ const RestaurantLayout = () => {
                 "https://th.bing.com/th/id/OIP.b-6A7J2ZuL4tyP33bRUfUgHaEw?w=273&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7"
 
             ]}/>
+            <RestaurantInfo 
+            name="Mumbai Express"
+            restaurantRating="3.8"
+            deliveryRating="3.5"
+            cuisine="Fast Food, North Indian,Chinese,Street Food"
+            address="bhubaneswar"/>
+            <div className="my-4 flex flex-wrap gap-3">
+                <InfoButtons isActive>
+                    <TiStarOutline /> Add Review
+                </InfoButtons>
+                <InfoButtons>
+                    <RiDirectionLine /> Direction
+                </InfoButtons>
+                <InfoButtons>
+                    <BiBookmarkPlus /> Bookmark
+                </InfoButtons>
+                <InfoButtons>
+                    <RiShareForwardLine /> Share
+                </InfoButtons>
+            </div>
        </div>
         </>
     );
