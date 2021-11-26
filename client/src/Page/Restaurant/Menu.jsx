7f7b61740b606b0a64cd7c7a0c5926c3cf32395a
ananya-01 <ananya200120@gmail.com>
2021-11-26 09:23:06 +0530

    Add reducer to menu page

--- client/src/Page/Restaurant/Menu.jsx
+++ client/src/Page/Restaurant/Menu.jsx
@@ -1,16 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 
 //components
 import MenuCollection from "../../components/restaurant/MenuCollection";
 
+// redux
+import { getImage } from "../../Redux/Reducer/Image/Image.action";
 
 const Menu = () => {
-  const [menus, setMenus] = useState([
-    "https://b.zmtcdn.com/data/pictures/0/18930190/8b41602a27ac441ba909237947be73ec.jpg",
-    "https://b.zmtcdn.com/data/reviews_photos/faf/e76d6bc3df21781ca49c93dd4d4edfaf_1624699914.jpg",
-    "https://b.zmtcdn.com/data/dish_photos/e56/c6942fc1a954329fe4ed8bdf9a481e56.jpg",
-  ]);
+  const [menus, setMenus] = useState([]);
 
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+  );
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    if (reduxState) {
+      dispatch(getImage(reduxState?.menuImages)).then((data) => {
+        const images = [];
+        data.payload.image.images.map(({ location }) => images.push(location));
+        setMenus(images);
+      });
+    }
+  }, [reduxState]);
   return (
     <>
     <div className="flex flex-wrap gap-3">
