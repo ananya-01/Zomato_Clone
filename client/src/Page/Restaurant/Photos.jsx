b828ac0294ade21f88981884c50074a56b1d65ce
ananya-01 <ananya200120@gmail.com>
2021-11-26 09:29:35 +0530

    Add reducer to photos page

--- client/src/Page/Restaurant/Photos.jsx
+++ client/src/Page/Restaurant/Photos.jsx
@@ -1,36 +1,35 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
 import ImageViewer from "react-simple-image-viewer";
 
 //components
 import PhotoCollection from "../../components/restaurant/PhotosCollection";
 
+// redux 
+import { getImage } from "../../Redux/Reducer/Image/Image.action";
 
 const Photos = () => {
-  const [photos, setPhotos] = useState([
-    "https://b.zmtcdn.com/data/pictures/0/18930190/8b41602a27ac441ba909237947be73ec.jpg",
-    "https://b.zmtcdn.com/data/reviews_photos/faf/e76d6bc3df21781ca49c93dd4d4edfaf_1624699914.jpg",
-    "https://b.zmtcdn.com/data/dish_photos/e56/c6942fc1a954329fe4ed8bdf9a481e56.jpg",
-    "https://b.zmtcdn.com/data/pictures/0/18930190/8b41602a27ac441ba909237947be73ec.jpg",
-    "https://b.zmtcdn.com/data/reviews_photos/faf/e76d6bc3df21781ca49c93dd4d4edfaf_1624699914.jpg",
-    "https://b.zmtcdn.com/data/dish_photos/e56/c6942fc1a954329fe4ed8bdf9a481e56.jpg",
-    "https://b.zmtcdn.com/data/pictures/0/18930190/8b41602a27ac441ba909237947be73ec.jpg",
-    "https://b.zmtcdn.com/data/reviews_photos/faf/e76d6bc3df21781ca49c93dd4d4edfaf_1624699914.jpg",
-    "https://b.zmtcdn.com/data/dish_photos/e56/c6942fc1a954329fe4ed8bdf9a481e56.jpg",
-    "https://b.zmtcdn.com/data/pictures/0/18930190/8b41602a27ac441ba909237947be73ec.jpg",
-    "https://b.zmtcdn.com/data/reviews_photos/faf/e76d6bc3df21781ca49c93dd4d4edfaf_1624699914.jpg",
-    "https://b.zmtcdn.com/data/dish_photos/e56/c6942fc1a954329fe4ed8bdf9a481e56.jpg",
-
-  ]);
+  const [photos, setPhotos] = useState([]);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [currentImg, setCurrentImg] = useState(0);
 
-  const closeViewer = () => {
-    setIsMenuOpen(false);
-  };
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.selectedRestaurant.restaurant
+  );
+  const dispatch = useDispatch();
 
-  const openViewer = (index) => {
-    setIsMenuOpen(true);
-  };
+  useEffect(() => {
+    if (reduxState) {
+      dispatch(getImage(reduxState?.photos)).then((data) => {
+        const images = [];
+        data.payload.image.images.map(({ location }) => images.push(location));
+        setPhotos(images);
+      });
+    }
+  }, []);
+  const closeViewer = () => setIsMenuOpen(false);
+
+  const openViewer = () => setIsMenuOpen(true);
 
   return (
     <>
