673bfa8691c80b77b955d4b0d48a3808c1dd7238
ananya-01 <ananya200120@gmail.com>
2021-11-27 18:13:45 +0530

    Add Razorpay Payment

--- server/API/orders/index.js
+++ server/API/orders/index.js
@@ -14,17 +14,25 @@ Params          id
 Access          public
 Method          GET
 */
-Router.get("/:_id", passport.authenticate("jwt", {session:false}) ,async (req, res) => {
-    try{
-        const {_id} = req.params;
-        const getOrders = await OrderModel.findOne({user:_id});
-        if(!getOrders)
-        return res.status(404).json({error: "User not found"});
-        return res.status(200).json({orders: getOrders});
-    }catch(error){
-        return res.status(500).json({error: error.message});
+Router.get(
+    "/:_id",
+    passport.authenticate("jwt", { session: false }),
+    async (req, res) => {
+      try {
+        const { _id } = req.params;
+  
+        const getOrders = await OrderModel.findOne({ user: _id });
+  
+        if (!getOrders) {
+          return res.status(404).json({ error: "User not found" });
+        }
+  
+        return res.status(200).json({ orders: getOrders });
+      } catch (error) {
+        return res.status(500).json({ error: error.message });
+      }
     }
-});
+);
 
 /* 
 Route           /new/_id
@@ -33,23 +41,24 @@ Params          _id
 Access          public
 Method          POST
 */
-Router.post("/new/:_id", passport.authenticate("jwt", {session:false}) , async (req,res) => {
-    try{
-        const {_id} = req.params;
-        const {orderDetails} =req.body;
-        const addNewOrder = await OrderModel.findOneAndUpdate(
-            {
-                user:_id,
-            },{
-            $push:{orderDetails},
-            },{
-                new: true,
-            });
-        if(!addNewOrder)
-        return res.status(404).json({error: "User not found"});
-        return res.status(200).json({order: addNewOrder});
-    }catch(error){
-        return res.status(500).json({error: error.message});
+Router.post("/new", passport.authenticate("jwt"), async (req, res) => {
+    try {
+      const { _id } = req.session.passport.user._doc;
+      const { orderDetails } = req.body;
+  
+      const addNewOrder = await OrderModel.findOneAndUpdate(
+        {
+          user: _id,
+        },
+        {
+          $push: { orderDetails },
+        },
+        { new: true }
+      );
+  
+      return res.json({ order: addNewOrder });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
 });
 
