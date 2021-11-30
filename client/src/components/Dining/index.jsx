30225ee1750202758284236e9c355306cc62b84e
ananya-01 <ananya200120@gmail.com>
2021-11-30 13:57:55 +0530

    Add reducer to dining and nightlife page

--- client/src/components/Dining/index.jsx
+++ client/src/components/Dining/index.jsx
@@ -1,16 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 // components
 import DiningCarousal from "./DiningCarousal";
+import RestaurantCard from "../RestaurantCard";
 
 const Dining = (props) => {
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.restaurants
+  );
+
+  useEffect(() => {
+    reduxState.restaurants && setRestaurantList(reduxState.restaurants);
+  }, [reduxState.restaurants]);
+
   return (
     <div>
        <DiningCarousal {...props} />
       <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
         Dine-Out Restaurants in Bhubaneswar
       </h1>
-      
+      <div className="flex justify-between flex-wrap">
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard
+            {...restaurant}
+            key={restaurant._id}
+            whereIsthisres="asf"
+          />
+        ))}
+      </div>
     </div>
   );
 };
--- client/src/components/NightLife/index.jsx
+++ client/src/components/NightLife/index.jsx
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 // components
 import NightLifeCarousal from "./NightLifeCarousal";
+import RestaurantCard from "../RestaurantCard";
 
 const NightLife = () => {
+  const [restaurantList, setRestaurantList] = useState([]);
+
+  const reduxState = useSelector(
+    (globalStore) => globalStore.restaurant.restaurants
+  );
+
+  useEffect(() => {
+    reduxState.restaurants && setRestaurantList(reduxState.restaurants);
+  }, [reduxState.restaurants]);
+
   return (
     <div>
       <NightLifeCarousal />
       <h1 className="text-xl my-4 md:my-8 md:text-3xl md:font-semibold">
         Nightlife Restaurants in Bhubaneswar
       </h1>
+      <div className="flex justify-between flex-wrap">
+        {restaurantList.map((restaurant) => (
+          <RestaurantCard
+            {...restaurant}
+            key={restaurant._id}
+            whereIsthisres="asf"
+          />
+        ))}
+      </div>
     </div>
   );
 };
